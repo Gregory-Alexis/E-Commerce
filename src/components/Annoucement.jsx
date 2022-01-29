9eb40a6cf8a909ce2e60aff22f7689cecc03738d
Westindiess <gregoryalexiss@gmail.com>
2022-01-29 16:49:02 +0100

    Annoucement

--- src/components/Annoucement.jsx
+++ src/components/Annoucement.jsx
@@ -1,5 +1,22 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
 const Annoucement = () => {
-  return <div></div>;
+  return (
+    <Container>
+      Bonnes Affaires! Livraison gratuite sur les commandes de plus de 50 euros
+    </Container>
+  );
 };
 
 export default Annoucement;
