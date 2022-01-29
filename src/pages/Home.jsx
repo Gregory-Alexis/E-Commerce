01349f28fd5cf51064f733143e3fc27181032992
Westindiess <gregoryalexiss@gmail.com>
2022-01-29 16:50:32 +0100

    HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import Annoucement from "../components/Annoucement";
+import Categories from "../components/Categories";
+import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import NewsLetter from "../components/NewsLetter";
+import Products from "../components/Products";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
+      <Annoucement />
       <NavBar />
+      <Slider />
+      <Categories />
+      <Products />
+      <NewsLetter />
+      <Footer />
     </div>
   );
 };
