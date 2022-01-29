9d012730836eff2e9e55644b5258f80e756e5837
Westindiess <gregoryalexiss@gmail.com>
2022-01-29 16:50:21 +0100

    Slider

---
+++ src/components/Slider.jsx
@@ -0,0 +1,117 @@
+import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
+import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
+import { sliderItems } from "../data";
+import styled from "styled-components";
+import { useState } from "react";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background-color: coral;
+  position: relative;
+`;
+
+export const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.7;
+  z-index: 2;
+`;
+
+export const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+  transition: all 1.5s ease;
+`;
+export const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bg};
+`;
+export const ImageContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+export const Image = styled.img`
+  height: 80%;
+`;
+
+export const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+export const Title = styled.h1`
+  font-size: 70px;
+  text-transform: uppercase;
+`;
+export const Description = styled.p`
+  margin: 25px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+  text-transform: uppercase;
+`;
+export const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+
+  return (
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <ArrowBackIosNewOutlinedIcon />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg} key={item.id}>
+            <ImageContainer>
+              <Image src={item.img} alt="" />
+            </ImageContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Description>
+                Ne faites pas de compromis sur votre style.
+                <br /> Obtenez 30 % de réduction sur tout les nouveaux arrivage.
+              </Description>
+              <Button>Je veux voir</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <ArrowForwardIosOutlinedIcon />
+      </Arrow>
+    </Container>
+  );
+};
+
+export default Slider;
