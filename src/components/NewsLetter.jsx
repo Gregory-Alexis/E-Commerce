78b3c9ea8bf117e63ae89b7f090ff32441158be2
Westindiess <gregoryalexiss@gmail.com>
2022-01-30 19:37:01 +0100

    NewsLetter.jsx

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,5 +1,6 @@
 import { Send } from "@mui/icons-material";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60vh;
@@ -11,36 +12,38 @@ const Container = styled.div`
 `;
 const Title = styled.h1`
   font-size: 70px;
-  margin: 10px;
+  margin-bottom: 20px;
 `;
+
 const Description = styled.div`
   font-size: 24px;
   font-weight: 300;
   margin-bottom: 20px;
+  ${mobile({ textAlign: "center" })}
 `;
+
 const InputContainer = styled.div`
-  width: 30%;
+  width: 50%;
   height: 40px;
   background-color: white;
   display: flex;
   justify-content: space-between;
   border: 1px solid lightgray;
+  ${mobile({ width: "80%" })}
 `;
+
 const Input = styled.input`
   border: none;
   flex: 8;
   padding-left: 20px;
 `;
+
 const Button = styled.button`
   flex: 1;
   border: none;
   background-color: teal;
   color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 `;
-
 const NewsLetter = () => {
   return (
     <Container>
