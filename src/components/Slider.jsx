0f993887ca26f57f908d53e256b9efd211ea8371
Westindiess <gregoryalexiss@gmail.com>
2022-01-30 19:37:27 +0100

    Slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,16 +3,18 @@ import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutl
 import { sliderItems } from "../data";
 import styled from "styled-components";
 import { useState } from "react";
+import { mobile } from "../responsive";
 
-export const Container = styled.div`
+const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
+  overflow: hidden;
+  ${mobile({ display: "none" })}
 `;
 
-export const Arrow = styled.div`
+const Arrow = styled.div`
   width: 50px;
   height: 50px;
   background-color: #fff7f7;
@@ -27,48 +29,51 @@ export const Arrow = styled.div`
   right: ${(props) => props.direction === "right" && "10px"};
   margin: auto;
   cursor: pointer;
-  opacity: 0.7;
+  opacity: 0.5;
   z-index: 2;
 `;
 
-export const Wrapper = styled.div`
+const Wrapper = styled.div`
   height: 100%;
   display: flex;
-  transform: translateX(${(props) => props.slideIndex * -100}vw);
   transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
-export const Slide = styled.div`
+
+const Slide = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
   align-items: center;
   background-color: #${(props) => props.bg};
 `;
-export const ImageContainer = styled.div`
+
+const ImageContainer = styled.div`
   height: 100%;
   flex: 1;
 `;
-export const Image = styled.img`
+
+const Image = styled.img`
   height: 80%;
 `;
 
-export const InfoContainer = styled.div`
+const InfoContainer = styled.div`
   flex: 1;
   padding: 50px;
 `;
 
-export const Title = styled.h1`
+const Title = styled.h1`
   font-size: 70px;
-  text-transform: uppercase;
 `;
-export const Description = styled.p`
-  margin: 25px 0px;
+
+const Description = styled.p`
+  margin: 50px 0px;
   font-size: 20px;
   font-weight: 500;
   letter-spacing: 3px;
-  text-transform: uppercase;
 `;
-export const Button = styled.button`
+
+const Button = styled.button`
   padding: 10px;
   font-size: 20px;
   background-color: transparent;
